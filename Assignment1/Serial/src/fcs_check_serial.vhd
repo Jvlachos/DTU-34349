3b92377d14c95c3431ece3c3cf2f9d283347343b
Dimitris Vlachos <jvlachos99@gmail.com>
2025-03-04 14:29:18 +0100

    Working serial version

--- Assignment1/Serial/src/fcs_check_serial.vhd
+++ Assignment1/Serial/src/fcs_check_serial.vhd
@@ -1,8 +1,12 @@
 Library ieee; 
 USE ieee.std_logic_1164.all ;
+use IEEE.NUMERIC_STD.ALL;
 
 
 entity fcs_check_serial is 
+	generic (
+		FRAME_BITS : integer := 512
+	);
 	port (
 		clk            : in std_logic; -- system clock
 		reset          : in std_logic; -- asynchronous reset
@@ -16,58 +20,90 @@ end fcs_check_serial;
 
 Library ieee; 
 USE ieee.std_logic_1164.all ;
+use IEEE.NUMERIC_STD.ALL;
 
-
-entity D_FF is
-		port (
-			D_in : in  std_logic;
-			CLK  : in  std_logic;
-			RST  : in  std_logic;
-			Q_out: out std_logic
-		);
+entity Regs is
+	generic (
+		REG_NUM : integer := 32
+	);
+	port (
+			data_in 	: in  std_logic_vector(REG_NUM-1 downto 0);
+			clk  		: in  std_logic;
+			reset  	: in  std_logic;
+			data_out : out std_logic_vector(REG_NUM-1 downto 0)
+			);	
+	signal regFile : std_logic_vector(REG_NUM-1 downto 0);
 end entity;
 
 
 
 ARCHITECTURE Behavior OF fcs_check_serial IS
-	constant c_REG_NUM : integer := 32;
-	signal regFile : std_logic_vector(c_REG_NUM-1 downto 0);
-	signal Q_out : std_logic_vector(c_REG_NUM-1 downto 0);
-	signal qin   : std_logic;
-BEGIN
+	constant C_REG_NUM : integer := 32;
+	signal regFileOut	 : std_logic_vector(c_REG_NUM-1 downto 0);
+	signal Gx			 : std_logic_vector(c_REG_NUM-1 downto 0) := x"04C11DB7"; -- CRC-32 polynomial
+	signal g 			 : std_logic_vector(c_REG_NUM-1 downto 0);
+	signal bit_count   : unsigned (FRAME_BITS downto 0);
+	signal bitCountReg : unsigned (FRAME_BITS downto 0) := (others => '0');
+	signal compute_crc : std_logic := '0';
+	signal compl_en 	 : std_logic := '0';
+	signal data 		 : std_logic;
 	
-	gen_registers : for i in 0 to c_REG_NUM-1 generate
-		reg_instance : entity work.D_FF
+BEGIN
+
+	compute_crc <= '1' when (start_of_frame = '1' or compute_crc = '1') and (bit_count < FRAME_BITS) else '0';
+	compl_en    <= '1' when (bit_count < 32) or ( end_of_frame = '1') else '0'; 
+	data 			<= not data_in when compl_en = '1' else data_in;
+	fcs_error   <= '0' when (bit_count = FRAME_BITS and regFileOut = x"00000000") else '1';  
+	
+	g(0) <= regFileOut(0) when compute_crc = '0' else 
+			data xor regFileOut(C_REG_NUM-1) when Gx(0) = '1';
+	
+	gen_gAssign : for i in 1 to C_REG_NUM-1 generate
+		g(i) <= regFileOut(i) when compute_crc = '0' else 
+					regFileOut(i-1) xor regFileOut(C_REG_NUM-1) when Gx(i) = '1'
+					else regFileOut(i-1);
+	end generate;
+	
+	reg_instance : entity work.Regs
+			generic map (
+				REG_NUM => C_REG_NUM
+			
+			)
 			port map (
-				D_in => regFile(i),
-				CLK  => clk,
-				RST  => reset,
-				Q_out=> Q_out(i)
+				data_in 	=> g,
+				clk  		=> clk,
+				reset  	=> reset,
+				data_out => regFileOut
 			);
-	end generate gen_registers;
-	qin <= '1';
-	process(clk)
+	
+	
+	process(reset,clk)
 	begin
 		if(reset = '1') then
-			regFile <= (others => '0');
+			bitCountReg <= (others => '0');
 		elsif(rising_edge(clk)) then
-			regFile <= regFile (c_REG_NUM-2 downto 0 ) & qin;
+			if(compute_crc = '1') then
+				bitCountReg <= bitCountReg + 1;
+			else 
+				bitCountReg <= bitCountReg;
+			end if;
 		end if;
+		bit_count <= bitCountReg;
 	end process;
-	
-	
+
 END Behavior;
 
 
 	
-architecture Behavior of D_FF is
+architecture Behavior of Regs is
 begin
-	process(CLK)
+	process(reset,clk)
 	begin
-		if(RST = '1') then
-			Q_out <= '0';
-		elsif(rising_edge(CLK)) then
-			Q_out <= D_in;
+		if(reset = '1') then
+			regFile<= (others => '0');
+		elsif (rising_edge(clk)) then
+			regFile <= data_in;
 		end if;
 	end process;
+	data_out <= regFile;
 end Behavior;
--- Assignment1/Serial/src/fcs_check_serial_tb.vhd
+++ Assignment1/Serial/src/fcs_check_serial_tb.vhd
@@ -13,19 +13,26 @@ end fcs_check_serial_tb;
 architecture Testbench of fcs_check_serial_tb is
 	signal clk 				: std_logic := '0';
 	signal reset 	 		: std_logic := '1';
-	signal start_of_frame	: std_logic := '0';
-	signal end_of_frame		: std_logic := '0';
+	signal start_of_frame: std_logic := '0';
+	signal end_of_frame	: std_logic := '0';
 	signal data_in			: std_logic := '0';
-	signal fcs_error		: std_logic := '0'; 
-	
-begin
-	clk <= not clk after 1 ns;
-	reset <= '1', '0' after  5 ns;
+	signal fcs_error		: std_logic := '0';
+	constant ETH_FRAME_BYTES  : integer := 64;
+	constant FRAME_BITS_TOTAL : integer := ETH_FRAME_BYTES * 8;  -- 64 bytes * 8 bits
+	signal frame : std_logic_vector(FRAME_BITS_TOTAL-1 downto 0) := 
+       X"0010A47BEA8000123456789008004500002EB3FE000080110540C0A8002CC0A8000404000400001A2DE8000102030405060708090A0B0C0D0E0F1011E6C53DB2";
+	 
+    constant CHECKSUM_BITS 	: integer := 4 * 8;      -- 4 bytes * 8 bits
+	 constant CLOCK_PERIOD 		: time := 5 ns;
 
+	 
+begin
+	clk <= not clk after CLOCK_PERIOD;
+	reset <= '1', '0' after  CLOCK_PERIOD;
 	dut : entity work.fcs_check_serial 
 		port map(
-			clk 			=> clk,
-			reset			=> reset,
+			clk 				=> clk,
+			reset				=> reset,
 			start_of_frame	=> start_of_frame,
 			end_of_frame	=> end_of_frame,
 			data_in			=> data_in,
@@ -35,10 +42,19 @@ begin
 	stimulus:
 	process begin
 		wait until (reset = '0');
-		data_in <= '1';
-		wait for 2 ns;
-		data_in <= '0';
-		wait for 2 ns;
+		start_of_frame <= '1';
+		for i in FRAME_BITS_TOTAL-1 downto 0 loop
+			data_in <= frame(i);
+			if( i > 31 ) then
+				end_of_frame <= '0';
+			else 
+				end_of_frame <= '1';
+			end if;
+			wait until rising_edge(clk);
+		end loop;
+		
+		wait until rising_edge(clk);
+		assert false report "End of simulation" severity failure;
 		wait;
 	end process stimulus;
 end Testbench;
